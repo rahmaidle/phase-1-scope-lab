dd983b23673fa822fde0ed07fe3cfb6e6256c95c
rahmaidle <rahmaidle254@gmail.com>
2024-06-12 14:01:58 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+window.customerName = 'bob';
+
+function upperCaseCustomerName() {
+  window.customerName = window.customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+const LEAST_FAVORITE_CUSTOMER = 'not liked';
+
+function changeLeastFavoriteCustomer() {
+  LEAST_FAVORITE_CUSTOMER = 'new least favorite'; // this will throw an error
+}
